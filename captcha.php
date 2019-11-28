338b643966309f106a54905e19efec7a32fc9c25
Romain Lebbadi-Breteau <romain@lebbadi.fr>
2019-11-27 22:28:44 -0400

    also send if new status at beginning of new hour

--- captcha.php
+++ captcha.php
@@ -46,7 +46,7 @@ $active = explode('<div class="path-item-label">', explode("path-item-active", $
 $active = trim(html_entity_decode(explode("</div>", $active)[0], ENT_QUOTES)) . "\n";
 echo $active;
 
-if(!strpos($active, "Demande en cours")) {
+if(!strpos($active, "Demande en cours") || date("i") == "00") {
     curl_setopt($ch, CURLOPT_URL, "https://smsapi.free-mobile.fr/sendmsg?user=" . getenv("SMS_USER") . "&pass=" . getenv("SMS_PASS") . "&msg=" . urlencode($active));
     curl_setopt($ch, CURLOPT_POST, false);
     curl_exec($ch);
